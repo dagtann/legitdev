b576a861ae0d7448d252078621decf6d4a0df8ae
Dag Tanneberg <dag@197.7.174.193.dyn.wzb.eu>
2016-08-18 11:31:11 +0200

    Setup core routines

---
+++ r/buildData/01_extractRegimeTypeData.R
@@ -0,0 +1,72 @@
+# This script pulls regime type data from the original
+# manuscript DS and converts into a clean format.
+# provide core dataset head ================================
+library('foreign')
+original_data <- read.dta(                    # stata import
+  file.path(
+    pathData,
+    'Kailitz, Wurster, Autocratic Regime legitimation and social policy outcomes april 2016 kai.dta'
+  )
+)
+stata_import_attributes <- c(
+  "datalabel", "time.stamp", "formats", "types",
+  "val.labels", "var.labels", "version", "label.table",
+  "expansion.table", "expansion.fields"
+)
+attributes(original_data)[stata_import_attributes] <- NULL
+str(original_data); detach(package:foreign)
+
+base <- select(
+  original_data,
+  cowcode, year, d_electoralautocracy,
+  d_militaryregime, d_monarchy, d_onepartyautocracy,
+  d_communistideocracy, d_personalistregime
+)
+str(base); rm(original_data)
+
+# preliminary tests on data structure ======================
+with(base,                            # panel uniquely id'd?
+  table(duplicated(paste(cowcode, year, sep = ':')))
+) # Yes
+                            # each panel single regime type?
+regime_type_dummies <- grep(pattern = 'd_', names(base), fixed = TRUE)
+summary(rowSums(base[, regime_type_dummies]))
+# max 1, but unexpected NA's
+summary(base[regime_type_dummies]) # identical # of NA's
+write.csv2(base, file.path(pathOut, 'base.csv'), row.names = FALSE)
+# NA's == democracy
+base <- filter(base, !is.na(d_electoralautocracy))
+
+# convert regime types to factor ===========================
+base <- within(base, {
+  regime_type <- ifelse(
+    d_electoralautocracy == 1, 1, ifelse(
+      d_militaryregime == 1, 2, ifelse(
+        d_monarchy == 1, 3, ifelse(
+          d_onepartyautocracy == 1, 4, ifelse(
+            d_communistideocracy == 1, 5, ifelse(
+              d_personalistregime == 1, 6, NA
+            )
+          )
+        )
+      )
+    )
+  )
+  regime_type <- factor(regime_type,
+    levels = sort(unique(regime_type)),
+    labels = c(
+      'Electoral', 'Military', 'Monarchy', 'Oneparty',
+      'Ideocracy', 'Personalist'
+    ),
+    ordered = FALSE
+  )
+  }
+)
+table(base$regime_type[base$year %in% 1960:2010])
+# matches 1960 - 2010 raw counts in manuscript
+
+# Housekeeping =============================================
+cleanWorkSpace <- c(cleanWorkSpace, 'base')
+rm(list = ls()[ls() %in% cleanWorkSpace == FALSE])
+save.image(file.path(pathOut, 'base.RData'))
+## END
---
+++ r/setup.R
@@ -0,0 +1,33 @@
+# Configure workspace ======================================
+rm(list = ls())
+if(Sys.info()[['user']]=='dag'){
+  pathData <- '/Users/dag/github/legitdev/data'
+  pathOut <- '/Users/dag/github/legitdev/out'
+  pathCode <- '/Users/dag/github/legitdev/r'
+  options(help_type = 'html')
+}
+
+# load packages and fonts ----------------------------------
+basicPacks <- c(
+  'RSQLite', 'dplyr', 'tidyr', 'ggplot2', 'extrafont'
+)
+stopifnot(            # Are all required packages installed?
+  any(basicPacks %in% installed.packages()[, 'Package'])
+)
+invisible(
+  lapply(basicPacks, library, character.only = TRUE)
+)
+rm(basicPacks)
+
+loadfonts(quiet=TRUE)
+
+plot.size <- 7
+
+# set convenience hooks ====================================
+cleanWorkSpace <- c(ls(), 'cleanWorkSpace')
+
+# Execute specialized scripts ==============================
+
+## Build data ----------------------------------------------
+source(file.path(pathCode, 'buildData', '01_extractRegimeTypeData.R'))
+ls()
